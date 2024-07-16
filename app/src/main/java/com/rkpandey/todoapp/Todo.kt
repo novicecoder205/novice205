62be962225a8cab60f460eddd1f40e0e00213cf7
novicecoder205 <warpick2000@gmail.com>
2024-07-16 16:53:41 +0700

    Test trial of an old file

---
+++ app/src/main/java/com/rkpandey/todoapp/Todo.kt
@@ -0,0 +1,16 @@
+package com.rkpandey.todoapp
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+import java.util.Date
+
+@Entity
+data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    var title: String,
+    var createdAt: Date
+)
